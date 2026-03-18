03b57db25817f3e268781f9d37a4a7f4bdaa38eb
Yogapmana <yogapermana402@gmail.com>
2026-03-18 18:45:48 +0700

    fix error

--- src/states/authUser/__tests__/reducer.test.js
+++ src/states/authUser/__tests__/reducer.test.js
@@ -11,7 +11,7 @@ describe("authUserReducer function", () => {
     const nextState = authUserReducer(initialState, action);
 
     // assert
-    expect(nextState).toEqual("Error");
+    expect(nextState).toEqual(initialState);
   });
 
   it("should return the authUser when given by setAuthUser action", () => {
